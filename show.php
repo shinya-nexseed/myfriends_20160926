3c709b415fe0c22b9d28f776af1495471c83500c
shinya-nexseed <shirai@nexseed.net>
2016-10-19 11:53:24 +0800

    show.phpとの連携まで

--- show.php
+++ show.php
@@ -1,41 +1,25 @@
 <?php
-    // ① DBへ接続
+    // DBへの接続
     $dsn = 'mysql:dbname=myfriends;host=localhost';
     $user = 'root';
     $password = '';
     $dbh = new PDO($dsn, $user, $password);
     $dbh->query('SET NAMES utf8');
-    // ② DBからareasテーブルの情報を取得
-    $sql = 'SELECT * FROM `areas` WHERE 1';
-    // DB名、テーブル名、カラム名はアクサングラーブで囲う
-      // SQL文の中では省略可
+    // areasテーブルからパラメータのarea_idを使用してデータ1レコードを取得
+    // $_GET = array('area_id'=>20);
+    $area_id = $_GET['area_id'];
+
+    $sql = 'SELECT `area_name` FROM `areas` WHERE `area_id` = ' . $area_id;
 
     $stmt = $dbh->prepare($sql);
     $stmt->execute();
 
-    // $recordデータ格納用の配列
-    $areas = array();
+    // 1レコード分しか取得しないのでfetchも1回で良い
+    $area = $stmt->fetch(PDO::FETCH_ASSOC);
 
-    // ③ 取得したareasテーブルの情報を表示
-    while(1) {
-        // $recordはareasテーブルのレコード1件を格納した連想配列
-          // $record = array('area_id' => 2 ,'area_name' => '青森県');
-        $record = $stmt->fetch(PDO::FETCH_ASSOC);
-        if ($record == false) {
-            break;
-        }
-
-        $areas[] = $record;
-        // echo $record['area_name'];
-        // echo '<br>';
-    }
-    // var_dump($areas);
     echo '<br>';
     echo '<br>';
-    echo '<br>';
-    echo count($areas);
-
-    $dbh = null;
+    // var_dump($area);
 
  ?>
 
@@ -61,6 +45,7 @@
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
   </head>
   <body>
   <nav class="navbar navbar-default navbar-fixed-top">
@@ -88,33 +73,48 @@
   <div class="container">
     <div class="row">
       <div class="col-md-4 content-margin-top">
-      <legend>都道府県一覧</legend>
-        <table class="table table-striped table-bordered table-hover table-condensed">
+      <legend><?php echo $area['area_name']; ?>の友達</legend>
+      <div class="well">男性：2名　女性：1名</div>
+        <table class="table table-striped table-hover table-condensed">
           <thead>
             <tr>
-              <th><div class="text-center">id</div></th>
-              <th><div class="text-center">県名</div></th>
-              <th><div class="text-center">人数</div></th>
+              <th><div class="text-center">名前</div></th>
+              <th><div class="text-center"></div></th>
             </tr>
           </thead>
           <tbody>
-            <!-- id, 県名を表示 -->
-            <?php foreach ($areas as $area) : ?>
-              <!-- $area = array('area_id'=>1, 'area_name'=>'北海道'); -->
+            <!-- 友達の名前を表示 -->
             <tr>
-              <td><div class="text-center"><?php echo $area['area_id']; ?></div></td>
+              <td><div class="text-center">山田　太郎</div></td>
               <td>
                 <div class="text-center">
-                  <a href="show.php?area_id=<?php echo $area['area_id'] ?>">
-                    <?php echo $area['area_name']; ?>
-                  </a>
+                  <a href="edit.html"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
                 </div>
               </td>
-              <td><div class="text-center">3</div></td>
             </tr>
-            <?php endforeach; ?>
+            <tr>
+              <td><div class="text-center">小林　花子</div></td>
+              <td>
+                <div class="text-center">
+                  <a href="edit.html"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td><div class="text-center">佐藤　健</div></td>
+              <td>
+                <div class="text-center">
+                  <a href="edit.html"><i class="fa fa-pencil"></i></a>&nbsp;&nbsp;&nbsp;&nbsp;
+                  <a href="javascript:void(0);" onclick="destroy();"><i class="fa fa-trash"></i></a>
+                </div>
+              </td>
+            </tr>
           </tbody>
         </table>
+
+        <input type="button" class="btn btn-default" value="新規作成" onClick="location.href='new.html'">
       </div>
     </div>
   </div>
